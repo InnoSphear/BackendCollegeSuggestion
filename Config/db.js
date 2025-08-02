43501a71d05716f4cf3b2a9c19b5db2d05497822
InnoSphear <innosphear@gmail.com>
2025-08-02 21:42:20 +0530

    Update db.js

--- Config/db.js
+++ Config/db.js
@@ -3,9 +3,7 @@ import mongoose from 'mongoose';
 const connectDb = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 10000,
+     
     });
     console.log('MongoDB Connected');
   } catch (error) {
